c01a38ddbdca44ac50b8e97dbfb550d8cff2a0f5
lyju777 <lyju777@hanmail.net>
2023-03-08 22:37:54 +0900

    axios 인터셉터로 token값 저장하기

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+function createInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+  return setInterceptors(instance);
+}
+const instance = createInstance();
 
 function registerUser(userData) {
   return instance.post('signup', userData);
