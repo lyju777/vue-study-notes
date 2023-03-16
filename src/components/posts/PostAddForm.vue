3904461795a8572edadcdbd2d53208bb06187d9c
lyju777 <lyju777@hanmail.net>
2023-03-16 23:03:46 +0900

    생성페이지 생성 및  api 연결

---
+++ src/components/posts/PostAddForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="contents">
+    <h1 class="page-header">Create post</h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title:</label>
+          <input id="title" type="text" v-model="title" />
+        </div>
+
+        <div>
+          <label for="contents">Contents:</label>
+          <textarea type="text" id="contents" rows="5" v-model="contents" />
+          <p v-if="!isContentsValid" class="validation-text warning">
+            Contents must be less than 200
+          </p>
+        </div>
+        <button type="submit" class="btn">Create</button>
+      </form>
+      <p class="log">
+        {{ logMessage }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createPost } from '@/api/index';
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+      logMessage: '',
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+    async submitForm() {
+      try {
+        const response = await createPost({
+          title: this.title,
+          contents: this.contents,
+        });
+        console.log(response);
+      } catch (error) {
+        console.log(error.response.data.message);
+        this.logMessage = error.response.data.message;
+      }
+    },
+  },
+
+  computed: {
+    isContentsValid() {
+      return this.contents.length <= 200;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+
+.btn {
+  color: white;
+}
+</style>
--- src/views/PostAddPage.vue
+++ src/views/PostAddPage.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h1>생성페이지</h1>
+    <PostAddForm></PostAddForm>
   </div>
 </template>
 
 <script>
+import PostAddForm from '@/components/posts/PostAddForm.vue';
+
 export default {
+  components: {
+    PostAddForm,
+  },
   data() {
     return {};
   },
